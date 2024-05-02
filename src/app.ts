2118c5b0006adc12f714f83d8896090886da4034
BrokiDev <bryantramirez855@gmail.com>
2024-05-02 17:52:06 -0400

    [feature]- adding cors configuration

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,14 @@ export const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: process.env.FRONTEND_URL,
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/auth', userRouter);
 
